7aeda6cd9b30d45344b0813e2e2a7495f611bc00
Peter Runge <prunge@causal.net.au>
2023-03-06 19:35:23 +1100

    Works with Maven 3.2 as well.

--- src/it/maven-multiversion-extension-by-class-path-lib/verify.groovy
+++ src/it/maven-multiversion-extension-by-class-path-lib/verify.groovy
@@ -1,6 +1,6 @@
 //Each different Maven version tested generated a different file name, check each one
 
-['3.9.0', '3.8.7', '3.6.3' , '3.5.4', '3.3.9'].each { mavenVersion ->
+['3.9.0', '3.8.7', '3.6.3' , '3.5.4', '3.3.9', '3.2.5'].each { mavenVersion ->
 
     //Check that the file downloaded by the dependency plugin using credentials from servers in settings.xml
     //that use username, password and token all encrypted which must be decrypted by our extension
